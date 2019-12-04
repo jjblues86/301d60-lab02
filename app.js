fa5ee57b21d90925525f7a6d11b4a0aa2870994d
Jerome Joof <jeromejoof@yahoo.com>
2019-12-04 11:41:02 -0800

    styled our application

--- app.js
+++ app.js
@@ -24,21 +24,25 @@ Images.prototype.renderClonedImages = function(){
   clone.find('h2').text(this.title);
   clone.find('img').attr('src', this.image_url);
   clone.find('img').attr('alt', this.keyword);
-
   clone.find('p').text(this.description);
+  clone.attr('class', this.keyword);
+
+  clone.removeAttr('class');
 
   $('main').append(clone);
 }
+
+
 
 $.get('data.json').then(
   (data) => {
     data.forEach(allImagesFile => {
       let images = new Images(allImagesFile.title, allImagesFile.image_url, allImagesFile.description, allImagesFile.keyword);
-      // allImages.push(images);
       images.renderClonedImages();
-      // images.renderSelect();
       images.optionMenu();
     })
+    $('.photo-images').hide();
+
   });
 
 // creating the option
@@ -72,13 +76,3 @@ $('select[name="horn-images"]').on('change', function() {
   $(`img[alt="${$selection}"]`).show()
 })
 
-
-// Images.prototype.renderSelect = function(){
-
-//   let option = $(`option`).clone();
-
-//   option.attr('value', this.keyword);
-//   option.text(this.keyword);
-
-//   $(`select`).append(option);
-// }
