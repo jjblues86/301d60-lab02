2ff6973169a9ce920f14cc211cddd0948c0f00f4
Jerome Joof <jeromejoof@yahoo.com>
2019-12-04 11:04:59 -0800

    images are now filtering

--- README.md
+++ README.md
@@ -8,18 +8,21 @@ Start time: _____9:15am
 Finish time: __9:54am___
 
 Actual time needed to complete: ___40mins__
+
 =============================================================================================================================
 
 
-Number and name of feature: ________________________________
+Number and name of feature: ______________Filter images__________________
 
-Estimate of time needed to complete: _____
+Estimate of time needed to complete: ___30mins__
 
-Start time: _____
+Start time: ___9:55am__
 
-Finish time: _____
+Finish time: _11:03am____
 
-Actual time needed to complete: _____
+Actual time needed to complete: ____68mins_
+
+=================================================================================================================================
 
 
 Number and name of feature: ________________________________
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const allImages = [];
+const keys = [];
 
 
 // creating a constructor for all the images
@@ -22,6 +23,8 @@ Images.prototype.renderClonedImages = function(){
 
   clone.find('h2').text(this.title);
   clone.find('img').attr('src', this.image_url);
+  clone.find('img').attr('alt', this.keyword);
+
   clone.find('p').text(this.description);
 
   $('main').append(clone);
@@ -31,7 +34,51 @@ $.get('data.json').then(
   (data) => {
     data.forEach(allImagesFile => {
       let images = new Images(allImagesFile.title, allImagesFile.image_url, allImagesFile.description, allImagesFile.keyword);
+      // allImages.push(images);
       images.renderClonedImages();
-    });
+      // images.renderSelect();
+      images.optionMenu();
+    })
   });
 
+// creating the option
+Images.prototype.optionMenu = function () {
+  if( keys.indexOf( this.keyword ) === -1 ){
+    $('select').append('<option class = "option"></option>');
+    let $option = $('option[class="option"]');
+
+    $option.attr('value', this.keyword);
+    $option.text(this.keyword);
+
+    $option.removeClass('option');
+
+    keys.push(this.keyword);
+  }
+};
+
+// //selecting box filtering
+$('select[name="horn-images"]').on('change', function() {
+  let $selection = $(this).val();
+  console.log($selection);
+
+  if($selection === 'default') {
+    $('img').show();
+    return;
+  }
+
+  $('h2').hide()
+  $('img').hide()
+  $('p').hide()
+  $(`img[alt="${$selection}"]`).show()
+})
+
+
+// Images.prototype.renderSelect = function(){
+
+//   let option = $(`option`).clone();
+
+//   option.attr('value', this.keyword);
+//   option.text(this.keyword);
+
+//   $(`select`).append(option);
+// }
